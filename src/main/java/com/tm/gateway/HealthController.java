744108d376a07f1fdee01659e2a73d1c0657c9c8
amit-dusane <amit.dusane@turtlemint.com>
2025-04-28 19:47:51 +0530

    add lombok

--- src/main/java/com/tm/gateway/HealthController.java
+++ src/main/java/com/tm/gateway/HealthController.java
@@ -1,13 +1,16 @@
 package com.tm.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
+        log.info("in health endpoint");
         return ResponseEntity.ok("Spring Cloud Gateway is up and running");
     }
 }

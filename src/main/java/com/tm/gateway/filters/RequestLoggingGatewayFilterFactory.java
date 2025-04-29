7ecb9e8eee89e72109db6092d937b599fb88af22
amit-dusane <amit.dusane@turtlemint.com>
2025-04-29 12:35:18 +0530

    add gateway config

--- src/main/java/com/tm/gateway/filters/RequestLoggingGatewayFilterFactory.java
+++ src/main/java/com/tm/gateway/filters/RequestLoggingGatewayFilterFactory.java
@@ -5,32 +5,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 public class RequestLoggingGatewayFilterFactory extends AbstractGatewayFilterFactory<RequestLoggingGatewayFilterFactory.Config> {
-
     private static final Logger logger = LoggerFactory.getLogger(RequestLoggingGatewayFilterFactory.class);
 
     public RequestLoggingGatewayFilterFactory() {
         super(Config.class);
+        logger.info("RequestLoggingGatewayFilterFactory initialized");
     }
 
     @Override
     public GatewayFilter apply(Config config) {
+        logger.info("RequestLoggingGatewayFilterFactory apply() method invoked");
         return (exchange, chain) -> {
             String requestPath = exchange.getRequest().getURI().getPath();
             logger.info("Request Path: {}", requestPath);
-
             return chain.filter(exchange).then(
-                    exchange.getResponse().setComplete().doOnSuccess(aVoid ->
-                            logger.info("Response completed for: {}", requestPath)
-                    )
+                    Mono.fromRunnable(() -> logger.info("Response completed for: {}", requestPath))
             );
         };
     }
 
-    // Configuration class for the filter (optional)
     public static class Config {
-        // Add configuration properties if needed
+        // Add custom configurations here if needed
     }
 }
+
